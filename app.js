0cba56e05da22a5b8fbe3d51d772fac084d0404f
Victor Aguilar <victorhugo.webdeveloper@gmail.com>
2020-09-19 22:49:32 -0500

    Creando estructura de proyecto

---
+++ app.js
@@ -0,0 +1,15 @@
+//Requires
+const fs = require('fs');
+
+
+let base = 7
+let data = ''
+
+for(let i=1; i <= 10; i++) {
+  data += `${base} * ${i} = ${base * i}\n`
+}
+
+fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+  if (err) throw err;
+  console.log(`El archivo tabla-${base}.txt ha sido creado`);
+});
