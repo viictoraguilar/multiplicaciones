696f9ff6a4d67a0e34825bc7d02aeea69067d101
Victor Aguilar <victorhugo.webdeveloper@gmail.com>
2020-09-20 10:46:27 -0500

    Organizando archivos

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-const {crearArchivo} = require('./multiplicar/multiplicar')
+const argv = require('./config/yargs').argv
+
+const {crearArchivo, listarTabla} = require('./multiplicar/multiplicar')
+
+let comando = argv._[0]
+
+switch(comando) {
+
+  case 'listar':
+    listarTabla(argv.base,argv.limite)
+  break
+
+  case 'crear':
+    crearArchivo(argv.base, argv.limite)
+      .then(archivo => console.log(`Archivo creado: ${archivo}`))
+      .catch(e => console.log(e))
+  break
+
+  default:
+    console.log('Comando no reconocido')
+
+}
 
 
-// console.log(process.argv)
-let argv = process.argv
-let parametro = argv[2]
-let base = parametro.split('=')[1]
+// console.log(argv.base)
+
+// let parametro = argv[2]
+// let base = parametro.split('=')[1]
 
 
-crearArchivo(base)
-  .then(archivo => console.log(`Archivo creado: ${archivo}`))
-  .catch(e => console.log(e))
--- multiplicar/multiplicar.js
+++ multiplicar/multiplicar.js
@@ -1,7 +1,15 @@
 //Requires
 const fs = require('fs');
 
-let crearArchivo = (base) => {
+let listarTabla = (base,limite=10) => {
+
+  for(let i=1; i <= limite; i++) {
+    console.log(`${base} * ${i} = ${base * i}`)
+  }
+
+}
+
+let crearArchivo = (base, limite=10) => {
 
   return new Promise((resolve,reject) => {
 
@@ -12,16 +20,16 @@ let crearArchivo = (base) => {
     
     let data = ''
     
-    for(let i=1; i <= 10; i++) {
+    for(let i=1; i <= limite; i++) {
       data += `${base} * ${i} = ${base * i}\n`
     }
     
-    fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+    fs.writeFile(`tablas/tabla-${base}-al-${limite}.txt`, data, (err) => {
 
       if (err) 
         reject(err)
       else
-        resolve(`tabla-${base}.txt`)
+        resolve(`tabla-${base}-al-${limite}.txt`)
     
     })
   
@@ -30,5 +38,6 @@ let crearArchivo = (base) => {
 }
 
 module.exports = {
-  crearArchivo
+  crearArchivo,
+  listarTabla
 }
