1eaba4be1333547ba75f5325db9406363c601ad5
Victor Aguilar <victorhugo.webdeveloper@gmail.com>
2020-09-19 23:20:04 -0500

    Añadiendo primera logica a archivos

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-//Requires
-const fs = require('fs');
+const {crearArchivo} = require('./multiplicar/multiplicar')
 
 
-let base = 7
-let data = ''
+// console.log(process.argv)
+let argv = process.argv
+let parametro = argv[2]
+let base = parametro.split('=')[1]
 
-for(let i=1; i <= 10; i++) {
-  data += `${base} * ${i} = ${base * i}\n`
-}
 
-fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
-  if (err) throw err;
-  console.log(`El archivo tabla-${base}.txt ha sido creado`);
-});
+crearArchivo(base)
+  .then(archivo => console.log(`Archivo creado: ${archivo}`))
+  .catch(e => console.log(e))
